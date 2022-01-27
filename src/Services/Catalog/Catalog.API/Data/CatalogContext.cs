5899b0886589f58be190b5aaa91363d472868cb0
yerkewka <yerkewka@mail.ru>
2022-01-27 23:11:45 +0600

    Catalog context and data seeding implemented

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Configuration;
+using Catalog.API.Entities;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IOptions<DatabaseSettings> options)
+        {
+            var client = new MongoClient(options.Value.ConnectionString);
+            var database = client.GetDatabase(options.Value.DatabaseName);
+
+            Products = database.GetCollection<Product>(options.Value.CollectionName);
+
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
