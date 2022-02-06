978c5a2d4f3c72935ab84df18ee5a6336ef82348
yerkewka <y.serikbayev@ssa.group>
2022-02-07 00:15:33 +0600

    Ordering API implemented

---
+++ src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,59 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.DeleteOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+using System.Net;
+
+namespace Ordering.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{userName}", Name = nameof(GetOrders))]
+        [ProducesResponseType(typeof(IEnumerable<OrderDto>), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<OrderDto>>> GetOrders(string userName)
+        {
+            return Ok(await _mediator.Send(new GetOrdersListQuery(userName)));
+        }
+
+        // Testing purpose
+        [HttpPost(Name = nameof(CheckoutOrder))]
+        [ProducesResponseType(typeof(int), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<int>> CheckoutOrder([FromBody] CheckoutOrderCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+
+        [HttpPut(Name = nameof(UpdateOrder))]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> UpdateOrder([FromBody] UpdateOrderCommand command)
+        {
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}", Name = nameof(DeleteOrder))]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> DeleteOrder(int id)
+        {
+            await _mediator.Send(new DeleteOrderCommand(id));
+
+            return NoContent();
+        }
+    }
+}
