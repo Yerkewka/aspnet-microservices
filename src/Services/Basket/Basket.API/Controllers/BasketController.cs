276ac0fa9d01bee3b93ca52cac6769e1646d1ab3
yerkewka <y.serikbayev@ssa.group>
2022-03-26 15:41:37 +0600

    Basket checkout event publishing implemented

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,6 +1,9 @@
-﻿using Basket.API.Entities;
+﻿using AutoMapper;
+using Basket.API.Entities;
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using EventBus.Messages.Events;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
@@ -12,11 +15,19 @@ namespace Basket.API.Controllers
     {
         private readonly IBasketRepository _basketRepository;
         private readonly DiscountGrpcService _discountGrpcService;
+        private readonly IMapper _mapper;
+        private readonly IPublishEndpoint _publishEndpoint;
 
-        public BasketController(IBasketRepository basketRepository, DiscountGrpcService discountGrpcService)
+        public BasketController(
+            IBasketRepository basketRepository, 
+            DiscountGrpcService discountGrpcService, 
+            IMapper mapper,
+            IPublishEndpoint publishEndpoint)
         {
             _basketRepository = basketRepository;
             _discountGrpcService = discountGrpcService;
+            _mapper = mapper;
+            _publishEndpoint = publishEndpoint;
         }
 
         [HttpGet("{userName}", Name = nameof(GetBasket))]
@@ -48,6 +59,32 @@ namespace Basket.API.Controllers
             await _basketRepository.DeleteBasket(userName);
 
             return Ok();
+        }
+
+        [HttpPost("[action]")]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
+        {
+            // Get existing basket with total price
+            var basket = await _basketRepository.GetBasket(basketCheckout.UserName);
+
+            if (basket == null)
+            {
+                return BadRequest();
+            }
+
+            // Set total price on basket checkout eventMessage
+            var messageEvent = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
+            messageEvent.TotalPrice = basket.TotalPrice;
+
+            // Send checkout event to the rabbitmq
+            await _publishEndpoint.Publish(messageEvent);
+
+            // Remove the basket
+            await _basketRepository.DeleteBasket(basketCheckout.UserName);
+
+            return Accepted();
         }
     }
 }
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,31 +1,44 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var services = builder.Services;
 // Add services to the container.
-builder.Services.AddControllers();
+services.AddControllers();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
 });
 
-builder.Services.AddStackExchangeRedisCache(options =>
+services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = builder.Configuration.GetConnectionString("Redis");
 });
 
-builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
 {
     options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]);
 });
 
-builder.Services.AddScoped<IBasketRepository, BasketRepository>();
-builder.Services.AddScoped<DiscountGrpcService>();
+services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration["EventBusSettings:Host"]);
+    });
+});
+
+services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+services.AddScoped<IBasketRepository, BasketRepository>();
+services.AddScoped<DiscountGrpcService>();
 
 var app = builder.Build();
 
