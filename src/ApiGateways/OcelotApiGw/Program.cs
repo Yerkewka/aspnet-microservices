1347af1b76977840c0d09f8beb142452e2d889eb
yerkewka <y.serikbayev@ssa.group>
2022-03-27 13:12:39 +0600

    Ocelot configuration json files added to local env

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -3,6 +3,11 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var configuration = builder.Configuration;
+
+configuration.AddJsonFile($"ocelot.json", true, true);
+configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+
 var logging = builder.Logging;
 
 logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
@@ -10,6 +15,7 @@ logging.AddConsole();
 logging.AddDebug();
 
 var services = builder.Services;
+
 services.AddOcelot();
 
 var app = builder.Build();
