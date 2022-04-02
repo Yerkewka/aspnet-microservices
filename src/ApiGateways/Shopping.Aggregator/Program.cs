690c2044b3a05e5bf8cd67bed1b4106c119ff202
yerkewka <y.serikbayev@ssa.group>
2022-04-02 20:47:00 +0600

    Developed aggregator web project

---
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -0,0 +1,38 @@
+using Shopping.Aggregator.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(config =>
+{
+    config.BaseAddress = new Uri(builder.Configuration["ApiSettings:CatalogUrl"]);
+});
+builder.Services.AddHttpClient<IBasketService, BasketService>(config =>
+{
+    config.BaseAddress = new Uri(builder.Configuration["ApiSettings:BasketUrl"]);
+});
+builder.Services.AddHttpClient<IOrderService, OrderService>(config =>
+{
+    config.BaseAddress = new Uri(builder.Configuration["ApiSettings:OrderUrl"]);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
